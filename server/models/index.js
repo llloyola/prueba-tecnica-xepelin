84d04ca2b3465fe395afd94091c99d880d4240c9
Luis Loyola <llloyola@uc.cl>
2021-12-22 23:26:05 -0300

    changed config variable name

--- server/models/index.js
+++ server/models/index.js
@@ -2,12 +2,10 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 
-const Op = Sequelize.Op
-
 const DB_HOST = process.env.DB_HOST;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_DATABASE = process.env.DB_DATABASE;
+const DB_DATABASE = process.env.DB;
 //const DB_PORT = process.env.DB_PORT;
 
 const sequelize = new Sequelize(
